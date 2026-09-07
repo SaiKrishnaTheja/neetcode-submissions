50f219c831425abbecfda66f58cc8615b3348924
SaiKrishnaTheja <41283838+SaiKrishnaTheja@users.noreply.github.com>
2026-09-07 09:22:50 -0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group_dict={}
+        for word in strs:
+            data = [0]*26
+            for char in word.lower():
+                data[ord(char)-ord("a")]+=1
+            if tuple(data) not in group_dict:
+                group_dict[tuple(data)]=[word]
+            else:
+                group_dict[tuple(data)].append(word)
+        return list(group_dict.values())
+
+        
+
+
+        
